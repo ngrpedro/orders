47a1cf2ddc274b4e963fffa60cbca42dbd0a6968
Pedro Soares <ngrpedro22@gmail.com>
2023-02-21 19:34:03 -0300

    feat:: create product

--- src/components/ProductForm.tsx
+++ src/components/ProductForm.tsx
@@ -1,19 +1,58 @@
+import { api } from '@/lib/axios'
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const ProductInputs = z.object({
+  name: z.string(),
+  price: z.string(),
+})
+
+type ProductType = z.infer<typeof ProductInputs>
 
 const ProductForm = () => {
+  const { register, handleSubmit } = useForm<ProductType>()
+
+  const onSubmit = async (data: ProductType) => {
+    try {
+      await api.post('products', {
+        name: data.name,
+        price: data.price,
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div className='max-w-sm space-y-4'>
+    <form onSubmit={handleSubmit(onSubmit)} className='max-w-sm space-y-4'>
       <h2 className='text-xl font-bold'>Cadastro de produto</h2>
       <label className='block space-y-1'>
         <span className='text-base font-semibold block'>Nome</span>
-        <input type='text' className='px-4 py-2 rounded-md w-full' />
+        <input
+          type='text'
+          className='px-4 py-2 rounded-md w-full'
+          {...register('name')}
+        />
       </label>
 
       <label className='block space-y-1'>
         <span className='text-base font-semibold block'>Preço</span>
-        <input type='number' className='px-4 py-2 rounded-md w-full' />
+        <input
+          type='text'
+          className='px-4 py-2 rounded-md w-full'
+          {...register('price')}
+        />
       </label>
-    </div>
+
+      <button
+        type='submit'
+        className='px-4 py-2 rounded-md bg-purple-900 text-white font-semibold 
+                hover:opacity-90 transition-all'
+      >
+        Cadastrar
+      </button>
+    </form>
   )
 }
 
--- src/pages/api/products/index.ts
+++ src/pages/api/products/index.ts
@@ -1,5 +1,23 @@
+import { prisma } from '@/lib/prisma'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json(req.body)
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).end()
+  }
+
+  const { name, price } = req.body
+  const convertPrice = parseFloat(price)
+
+  const product = await prisma.product.create({
+    data: {
+      name,
+      price: convertPrice,
+    },
+  })
+
+  return res.status(201).json(product)
 }
