e7d8bb1de9482f2887a6354782f2829101a8b164
Pedro Soares <ngrpedro22@gmail.com>
2023-02-22 16:48:19 -0300

    styling getting  active route by layout navbar

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,12 +1,15 @@
 import React, { ReactNode } from 'react'
 import { SquaresFour, ShoppingCart, User } from 'phosphor-react'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 interface LayoutProps {
   children: ReactNode
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const router = useRouter()
+
   return (
     <>
       <header className='w-full bg-neutral-800 px-4 py-2 flex items-center justify-center'>
@@ -14,9 +17,26 @@ const Layout = ({ children }: LayoutProps) => {
       </header>
       <div className='h-screen px-4 py-8'>{children}</div>
       <nav className='sticky bottom-0 w-full bg-neutral-800 px-4 py-4 flex items-center justify-between'>
-        <SquaresFour size={24} />
-        <ShoppingCart size={24} className='text-green-700' />
-        <User size={24} />
+        <Link href={'/home'}>
+          <SquaresFour
+            size={24}
+            className={router.pathname === '/home' ? 'text-green-700' : ''}
+          />
+        </Link>
+        <Link href={'/shoppingCart'}>
+          <ShoppingCart
+            size={24}
+            className={
+              router.pathname === '/shoppingCart' ? 'text-green-700' : ''
+            }
+          />
+        </Link>
+        <Link href={'/profile'}>
+          <User
+            size={24}
+            className={router.pathname === '/profile' ? 'text-green-700' : ''}
+          />
+        </Link>
       </nav>
     </>
   )
