f8646150056e94a75074a750a7e78aa3ba987f19
Pedro Soares <ngrpedro22@gmail.com>
2023-02-24 15:34:10 -0300

    stylin:: init get loading products

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,7 +6,9 @@ import React from 'react'
 
 const Home = () => {
   const { products } = useProducts()
-  
+
+  console.log(products.length)
+
   return (
     <>
       <Head>
@@ -20,7 +22,7 @@ const Home = () => {
 
         {/* Categories */}
         <div className='space-y-3'>
-          <h2 className="md:text-2xl font-semibold">Categorias</h2>
+          <h2 className='md:text-2xl font-semibold'>Categorias</h2>
           <div className='grid grid-cols-5 md:grid-cols-9 gap-2'>
             <span className='rounded-full bg-neutral-700 w-16 h-16 md:w-28 md:h-28'></span>
             <span className='rounded-full bg-neutral-700 w-16 h-16 md:w-28 md:h-28'></span>
@@ -32,7 +34,7 @@ const Home = () => {
 
         {/* Promotions */}
         <div className='space-y-3'>
-          <h2 className="md:text-2xl font-semibold">Promoções</h2>
+          <h2 className='md:text-2xl font-semibold'>Promoções</h2>
           <div className='grid grid-cols-5 md:grid-cols-9 gap-2'>
             <span className='rounded-full bg-neutral-700 w-16 h-16 md:w-28 md:h-28'></span>
             <span className='rounded-full bg-neutral-700 w-16 h-16 md:w-28 md:h-28'></span>
@@ -41,12 +43,19 @@ const Home = () => {
 
         {/* Todoos os produtos */}
         <div className='space-y-3'>
-          <h2 className="md:text-2xl font-semibold">Todos os produtos</h2>
-          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5'>
-            {products.map((product) => {
-              return <ProductCard key={product.id} {...product}/>
-            })}
-          </div>
+          <h2 className='md:text-2xl font-semibold'>Todos os produtos</h2>
+
+          {products.length > 0 ? (
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5'>
+              {products.map((product) => {
+                return <ProductCard key={product.id} {...product} />
+              })}
+            </div>
+          ) : (
+            <div className='h-60 text'>
+              <p>Seus produtos aparecem aqui</p>
+            </div>
+          )}
         </div>
       </main>
     </>
